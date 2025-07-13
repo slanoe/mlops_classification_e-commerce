cf117affcce191b5b89dfc5aaf710c79f5224cdd
Steven Lanoë <stevenlanoe@gmail.com>
2025-07-13 16:46:30 +0200

    Fix github action

--- tests/test_api_fastapi.py
+++ tests/test_api_fastapi.py
@@ -3,11 +3,11 @@ import requests
 def test_add_product_predict_category():
     url = "http://localhost:8000/add_product"
     payload = {
-        "text": "Balancoire pour jardin extérieur",
-        "category": "2582"
+        "text": "Balancoire pour jardin exterieur",
+        "category": "1302"
     }
     response = requests.post(url, json=payload)
     assert response.status_code == 200
     data = response.json()
     assert "category" in data
-    assert str(data["category"]) == "2582"
+    assert str(data["category"]) == "1302"
--- tests/test_bento_service.py
+++ tests/test_bento_service.py
@@ -2,9 +2,9 @@ import requests
 
 def test_predict_balancoire():
     url = "http://localhost:3000/predict"
-    payload = {"text": "Balancoire pour jardin extérieur"}
+    payload = {"text": "Balancoire pour jardin exterieur"}
     response = requests.post(url, json=payload)
     assert response.status_code == 200
     data = response.json()
     assert "category" in data
-    assert str(data["category"]) == "2582"
+    assert str(data["category"]) == "1302"
