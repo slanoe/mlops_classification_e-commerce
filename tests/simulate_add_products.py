51f5289cf4cdbfe623aa191df06cc25076f33d50
Steven Lanoë <stevenlanoe@gmail.com>
2025-07-14 13:10:18 +0200

    Fix images build

--- tests/simulate_add_products.py
+++ tests/simulate_add_products.py
@@ -1,9 +1,12 @@
 import time
 import csv
 import requests
+import os
 
 CSV_FILE = "tests/simulate_add_products.csv"
 FASTAPI_URL = "http://localhost:8000"
+API_KEY = os.getenv("API_KEY", "mysecretkey")
+HEADERS = {"X-API-Key": API_KEY}
 
 with open(CSV_FILE, newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=",")
@@ -19,7 +22,11 @@ with open(CSV_FILE, newline='', encoding='utf-8') as csvfile:
         }
 
         try:
-            response = requests.post(f"{FASTAPI_URL}/add_product", json=payload)
+            response = requests.post(
+                f"{FASTAPI_URL}/add_product", 
+                json=payload,
+                headers=HEADERS
+            )
             print(f"[AJOUTÉ] {response.json()}")
         except Exception as e:
             print(f"[ERREUR] {e}")
